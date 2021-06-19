f80e6fa6aa3d4edf2e142f5f4bf60b051d940814
Vladislav Yakovlev <difencee@gmail.com>
2021-06-19 22:25:26 +0500

    added singleton in database config

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -8,21 +8,23 @@ public class DatabaseConfig {
     private final String DB_URL = "jdbc:postgresql://localhost:7777/usersdb";  // TODO: change this fields next time
     private final String USER = "postgres";
     private final String PASS = "...";
+    Connection connection;
 
     public Connection getConnection() {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
-            e.printStackTrace();
+        if (connection == null) {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                System.out.println("PostgreSQL JDBC Driver is not found. Include it in your library path ");
+                e.printStackTrace();
 
-        }
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(DB_URL, USER, PASS);
-        } catch (SQLException e) {
-            System.out.println("Connection Failed");
-            e.printStackTrace();
+            }
+            try {
+                connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            } catch (SQLException e) {
+                System.out.println("Connection Failed");
+                e.printStackTrace();
+            }
         }
         return connection;
     }
