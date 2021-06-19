43b4ee830e67b488b1d46878a97c16dbd10c55ff
Aleksey Zateev <alekseyzateev@gmail.com>
2021-06-19 10:14:11 +0500

    подготовил проект к работе

---
+++ src/main/java/dreamTeam/servlet/TestFile.java
@@ -0,0 +1,18 @@
+package dreamTeam.servlet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("test")
+//localhost:8085/rest/test
+public class TestFile {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Всем привет";
+    }
+}
+
