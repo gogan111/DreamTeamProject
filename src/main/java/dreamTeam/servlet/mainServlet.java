485a495055a7453c0fff34e9d5e3697642655102
AndreySinyaokiy <andrew.syniaokii@gmail.com>
2021-06-19 13:01:02 +0300

    update mainServlet

--- src/main/java/dreamTeam/servlet/mainServlet.java
+++ src/main/java/dreamTeam/servlet/mainServlet.java
@@ -3,16 +3,10 @@ package dreamTeam.servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class mainServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-    }
+
 
     @Override
     public void init(ServletConfig config) throws ServletException {
