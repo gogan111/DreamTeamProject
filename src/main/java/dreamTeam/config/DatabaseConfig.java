47cc676b24ce0f73ff221b834bdf063c84fc44bc
Vladislav Yakovlev <difencee@gmail.com>
2021-06-25 01:35:30 +0500

    another test aaaaa

--- src/main/java/dreamTeam/config/DatabaseConfig.java
+++ src/main/java/dreamTeam/config/DatabaseConfig.java
@@ -5,11 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    private final String DB_URL = "jdbc:postgresql://localhost:5432/alex";
+
     // TODO: change this fields next time
     //change DB_URL database userdb->alex
-    private final String USER = "alex";
-    private final String PASS = "1234";
+    private final String DB_URL = "jdbc:postgresql://37.139.171.136:5432/userdb";
+    private final String USER = "postgres";
+    private final String PASS = "admin";
+//     private final String DB_URL = "jdbc:postgresql://localhost:5432/alex";
+//     private final String USER = "alex";
+//     private final String PASS = "1234";
     Connection connection;
 
     public Connection getConnection() {
