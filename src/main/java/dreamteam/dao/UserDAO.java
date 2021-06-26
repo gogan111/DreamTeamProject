f03c852357a02375e17847ee0b25133b9e89e000
Aleksey Zateev <alekseyzateev@gmail.com>
2021-06-26 19:36:49 +0500

    send to cloudes

--- src/main/java/dreamteam/dao/UserDAO.java
+++ src/main/java/dreamteam/dao/UserDAO.java
@@ -103,5 +103,84 @@ public class UserDAO {
         }
         return null;
     }
+
+//    public int saveUser(User user) {
+//        String insert = "INSERT INTO users (name, surname, age, email) values (?, ?, ?, ?)";
+//        try (PreparedStatement preparedStatement = databaseConfig.getConnection()
+//                .prepareStatement(insert, Statement.RETURN_GENERATED_KEYS)) {
+//            preparedStatement.setString(1, user.getName());
+//            preparedStatement.setString(2, user.getSurname());
+//            preparedStatement.setInt(3, user.getAge());
+//            preparedStatement.setString(4, user.getEmail());
+//            preparedStatement.executeUpdate();
+//            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+//                while (resultSet.next()) {
+//                    return resultSet.getInt("id");
+//                }
+//            } catch (SQLException e) {
+//                throw new SQLException("Could not receive id of a user");
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return 0;
+//    }
+//
+//    public boolean updateUser(User user) {
+//        String updateUsr = "UPDATE users SET (name, surname, age, email) = (?, ?, ?, ?) WHERE email = ?";
+//        try (PreparedStatement preparedStatement = databaseConfig.getConnection().prepareStatement(updateUsr)) {
+//            preparedStatement.setString(1, user.getName());
+//            preparedStatement.setString(2, user.getSurname());
+//            preparedStatement.setInt(3, user.getAge());
+//            preparedStatement.setString(4, user.getEmail());
+//            preparedStatement.setString(5, user.getEmail());
+//            preparedStatement.executeUpdate();
+//            return true;
+//
+//        } catch (SQLException e) {
+//            System.err.println("Could not find a user");
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//
+//    /*
+//     * Пользователь удаляется по email
+//     * statement.setString (номер аттрибута в таблице, название аттрибута)
+//     */
+//
+//    public boolean deleteUser(String email) {
+//        String delete = "DELETE FROM users WHERE email = ?";
+//        try (PreparedStatement preparedStatement = databaseConfig.getConnection().prepareStatement(delete)) {
+//            preparedStatement.setString(1, email);
+//            preparedStatement.execute();
+//            return true;
+//        } catch (SQLException e) {
+//            System.err.println("Could not find a user");
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//
+//    public List<User> getAllUsers() {
+//        String query = "SELECT * FROM users ORDER BY email";
+//        try (PreparedStatement preparedStatement = databaseConfig.getConnection().prepareStatement(query);
+//             ResultSet resultSet = preparedStatement.executeQuery()) {
+//            List<User> usersList = new ArrayList<>();
+//            while (resultSet.next()) {
+//                user.setId(resultSet.getInt(1));
+//                user.setName(resultSet.getString(2));
+//                user.setSurname(resultSet.getString(3));
+//                user.setAge(resultSet.getInt(4));
+//                user.setEmail(resultSet.getString(5));
+//                usersList.add(user);
+//            }
+//            return usersList;
+//        } catch (SQLException e) {
+//            System.err.println("Could not get the list of the users");
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 }
 
