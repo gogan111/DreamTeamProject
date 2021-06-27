01b3925eef7e5470a860336b28e1b1a43b94cf17
Valerkovich <67497195+Valerkovich@users.noreply.github.com>
2021-06-27 19:48:37 +0300

    Update DatabaseConfig.java

--- src/main/java/dreamteam/config/DatabaseConfig.java
+++ src/main/java/dreamteam/config/DatabaseConfig.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConfig {
     private final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/userdb";
     private final String USER = "postgres";
-    private final String PASS = "admin";
+    private final String PASS = "12061996";
 //    private final String DB_URL = "jdbc:postgresql://localhost:5432/alex";
 //    private final String USER = "alex";
 //    private final String PASS = "1234";
