f3cace0ec73490dd0f3f0179cc27dcec7b9545af
Andrew <andrew.syniaokii@gmail.com>
2021-08-05 11:42:15 +0300

    fixed problem with CORS and path to database

--- src/main/java/dreamteam/controller/MainController.java
+++ src/main/java/dreamteam/controller/MainController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:9999")
 @RequestMapping("/users")
 class MainController {
     @Autowired
